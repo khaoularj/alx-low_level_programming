97bc34e0ec7644074cf9d3aa1cf2422fad978789
khaoularj <khaoula.raja1@gmail.com>
2023-03-16 10:15:33 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * main - the main function
  *
- * Return: 0
+ * Return:0
  */
 
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
