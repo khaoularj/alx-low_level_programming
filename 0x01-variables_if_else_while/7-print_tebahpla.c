50d82a20bbc339184163201bf8fdbe291eda808f
khaoularj <khaoula.raja1@gmail.com>
2023-03-20 16:19:42 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = 'z' ; j >= 'a' ; j--)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
+
+
