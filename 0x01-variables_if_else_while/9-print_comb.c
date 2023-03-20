ec3202d40e2fd121cac51896777f0652f2fc0b19
khaoularj <khaoula.raja1@gmail.com>
2023-03-20 16:54:33 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		if (n == 9)
+			putchar(n + '0');
+		else
+		{	putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
