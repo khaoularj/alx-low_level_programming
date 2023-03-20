f7782d18e9f04cf9b804bd70c132b7313e768a82
khaoularj <khaoula.raja1@gmail.com>
2023-03-20 15:08:09 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
