6fa504f9cf4a20ff1dbb5d4e9e7b1d5ee9ed8e24
khaoularj <khaoula.raja1@gmail.com>
2023-03-23 10:23:00 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - the function that adds two integers and returns the result
+ *@x: 1st parameter
+ *@y: 2nd parameter
+ * return: sum of x+y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ * times_table - the function that prints the 9 times table, starting with 0
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	for 
+}
