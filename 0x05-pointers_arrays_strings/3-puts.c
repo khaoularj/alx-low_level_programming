0fc3fa324913d433ef29086f2c8b07ebaa22c5cf
khaoularj <khaoula.raja1@gmail.com>
2023-03-27 16:36:32 -0700

    reverse

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,8 +9,8 @@
 void _puts(char *str)
 {
 	int x;
-
-	for (x = 0 ; str[x] != '\0' ; x++)
+	
+	for (x =  0; str[x] != ('\0'); x++)
 			_putchar(str[x]);
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - the function that prints a string, in reverse
+ * @s:parameter
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int x;
+	int count = 0;
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+		count++;
+	for (x = count ; x >= 0 ; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
