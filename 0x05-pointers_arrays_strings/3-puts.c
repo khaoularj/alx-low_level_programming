592ec64861eebfb407dd5360b5a523247db1aed7
khaoularj <khaoula.raja1@gmail.com>
2023-03-28 05:12:12 -0700

    _puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,8 +9,10 @@
 void _puts(char *str)
 {
 	int x;
-	
-	for (x =  0; str[x] != ('\0'); x++)
-			_putchar(str[x]);
+
+	for (x =  0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
 	_putchar('\n');
 }
