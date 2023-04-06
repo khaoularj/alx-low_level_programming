697af502bf38c2c2fa1cd9b596343345847be89f
khaoularj <khaoula.raja1@gmail.com>
2023-04-06 15:51:25 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,19 +9,15 @@ int is_prime_number(int n)
 	int x;
 
 	if (n <= 1)
-	{
 		return (0);
-	}
 	{
 	for (x = 2; x * x <= n; x++)
-	{
 	if (n % x == 0)
 	{
 		return (0);
 	}
 	}
 	return (1);
-	}
 }
 
 
