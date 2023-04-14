476763af6783b2c077415bd6ba26c45d52e224ed
khaoularj <khaoula.raja1@gmail.com>
2023-04-14 10:59:54 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,11 +18,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		return (NULL);
+		return ("");
 	}
 	if (s2 == NULL)
 	{
-		return (NULL);
+		return ("");
 	}
 
 	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
