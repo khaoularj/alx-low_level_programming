c35c99df5fe3bba458f5c17331e5e8d23ceb1ef4
khaoularj <khaoula.raja1@gmail.com>
2023-04-13 18:23:51 -0700

    array_range

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - the function that allocates memory for an array
+ * @nmemb:
+ * @size:
+ * Return: on success,  pointer to the allocated memory
+ * on fail, return NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
