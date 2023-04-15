b20e0d8b3413b1322f54179575a448d56b4bbd02
khaoularj <khaoula.raja1@gmail.com>
2023-04-15 15:31:40 -0700

    4add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
