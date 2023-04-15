114f8cd3f5db46f7afc665ce3831e771f668ef96
khaoularj <khaoula.raja1@gmail.com>
2023-04-15 15:14:16 -0700

    multiple

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -the function that multiplies two numbers
+ * @argc:argument count
+ * @argv:argument vector
+ * Return: on success 0, on fail, error
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+	return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+
+	printf("%d\n", result);
+	return (0);
+}
