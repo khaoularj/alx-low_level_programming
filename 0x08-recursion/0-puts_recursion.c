1054f2a9808f4fdc8a7e7ab8256c8ed046be3f3b
khaoularj <khaoula.raja1@gmail.com>
2023-06-07 14:21:32 -0700

    print a string with _puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - the function that prints a string
+ * @s: the string to be printed
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursive function is the case when a program allows you to call a function inside the same function
