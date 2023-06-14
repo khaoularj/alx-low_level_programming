d97a774fb762e4b8fc8f45213d4145637b12ca70
khaoularj <khaoula.raja1@gmail.com>
2023-06-14 10:52:32 -0700

    print the name of the function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the function that prints its name
+ * @argc:the argument count
+ * @argv:the argument vector
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
