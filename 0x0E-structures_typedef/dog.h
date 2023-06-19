f8b90daa5b7b0b4dfe86154e268a627b509e35c1
khaoularj <khaoula.raja1@gmail.com>
2023-06-19 08:12:22 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - structure that gives information about dog
+ *@name: the first member of struct
+ *@age: the second member of struct
+ *@owner: the third member of struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
