79c3b41d28a4470ac801c7f0ea704b0ee462ff9a
khaoularj <khaoula.raja1@gmail.com>
2023-07-07 13:20:31 -0700

    sets the value of a bit to 1

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * get_bit - the function that returns the value of a bit at a given index
+ * @n:the given number
+ * @index:the index of the bit
+ * Return: the value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
