90d0671deba941f6d4e1a91a8af93792c717887c
khaoularj <khaoula.raja1@gmail.com>
2024-01-16 01:21:11 -0800

    search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,13 +18,10 @@ int linear_search(int *array, size_t size, int value)
 
 	for (index = 0; index < size; index++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
 
 		if (array[index] == value)
-		{
 			return (index);
-		}
 	}
 	return (-1);
-printf("\n");
 }
